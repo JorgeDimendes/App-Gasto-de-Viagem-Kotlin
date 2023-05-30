7507ec96d236350bba75644e228ffc4dee7fa34e
Jorge <jorgedimendes@hotmail.com>
2023-05-30 11:46:13 -0300

    Adicionando uma novo comentario abaixo

--- app/src/main/java/com/jordev/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/jordev/gastoviagem/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //Jorge
+        //Jorge
 
     }
     fun calculate(){
