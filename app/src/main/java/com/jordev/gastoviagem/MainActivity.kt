da0bd717c510c77b6f77aa03df20118a4195e39e
Jorge <jorgedimendes@hotmail.com>
2023-05-30 11:29:49 -0300

    Adicionando uma nova imagem no site

---
+++ app/src/main/java/com/jordev/gastoviagem/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.jordev.gastoviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jordev.gastoviagem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+    fun calculate(){
+    }
+}
