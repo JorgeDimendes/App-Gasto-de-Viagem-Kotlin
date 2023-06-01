92fbbacfc110fa0f8069f5b32cbc61a7d3751a84
Jorge <jorgedimendes@hotmail.com>
2023-06-01 09:12:39 -0300

    Adicionado Override onClic

--- app/src/main/java/com/jordev/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/jordev/gastoviagem/MainActivity.kt
@@ -2,24 +2,27 @@ package com.jordev.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.jordev.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //projeto teste 01
-        //projeto teste 01
+
+        binding.buttonCalculate.setOnClickListener(this)
 
     }
+    override fun onClick(view: View) {
+        TODO("Not yet implemented")
+    }
+
     fun calculate(){
     }
-}
 
-    // Esse é um novo comentario da git checkout comentario_Animes
-    //Novo comentario para deixar atualizado 10:40
-    //Novo teste para subir 10:51
-    //Novo teste para subir 10:55
+    fun teste(view: View) {}
+
+}
